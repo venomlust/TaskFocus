22d3e726f131cfd62e6a03b65d82e9536d2427bb
Lucas Bertollo <lucaszbertollo@hotmail.com>
2016-08-28 11:23:49 -0300

    update

--- js/controller/scheduleCtrl.js
+++ js/controller/scheduleCtrl.js
@@ -1,6 +1,19 @@
 angular.module("appModule").controller("scheduleCtrl", function($scope, DbService) {
-  $scope.number = 31;
-  $scope.getNumber = function(num) {
-      return new Array(num);
-  }
+
+    $scope.date = {
+        month: {
+            onNumber: 1,
+            onString: ["January", "February", "March", "April", "May", "June",
+                "July", "August", "September", "October", "November", "December"
+            ]
+        },
+        year: 2016,
+        days: 31
+    };
+
+    $scope.getNumber = function(num) {
+        return new Array(num);
+    }
+
+
 });
